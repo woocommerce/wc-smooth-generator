de4c162c3f8c96ba610e445d3c547f77ee62c4d5
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2020-10-27 07:31:13 +0100

    Add extra space to conform to coding standards

--- wc-smooth-generator.php
+++ wc-smooth-generator.php
@@ -13,7 +13,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // autoloader.
-if( !class_exists( \WC\SmoothGenerator\Plugin::class ) ){
+if( ! class_exists( \WC\SmoothGenerator\Plugin::class ) ){
 	require __DIR__ . '/vendor/autoload.php';
 }
 
