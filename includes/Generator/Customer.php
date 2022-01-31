f0b331b25d4b5e566e753ac179df8f7c62a10966
Michael Schädler <michael@michaelschaedler.li>
2022-01-31 11:06:36 +0100

    added different shipping and billing addresses

--- includes/Generator/Customer.php
+++ includes/Generator/Customer.php
@@ -10,76 +10,139 @@ namespace WC\SmoothGenerator\Generator;
 /**
  * Customer data generator.
  */
-class Customer extends Generator {
+class Customer extends Generator
+{
 
-	/**
-	 * Return a new customer.
-	 *
-	 * @param bool $save Save the object before returning or not.
-	 * @return \WC_Customer Customer object with data populated.
-	 */
-	public static function generate( $save = true ) {
-		self::init_faker();
+    /**
+     * Return a new customer.
+     *
+     * @param  bool $save Save the object before returning or not.
+     * @return \WC_Customer Customer object with data populated.
+     */
+    public static function generate( $save = true )
+    {
+        self::init_faker();
 
-		// Make sure a unique username and e-mail are used.
-		do {
-			$username = self::$faker->userName();
-		} while ( username_exists( $username ) );
+        // Make sure a unique username and e-mail are used.
+        do {
+            $username = self::$faker->userName();
+        } while ( username_exists($username) );
 
-		do {
-			$email = self::$faker->safeEmail();
-		} while ( email_exists( $email ) );
+        do {
+            $email = self::$faker->safeEmail();
+        } while ( email_exists($email) );
 
-		$firstname   = self::$faker->firstName( self::$faker->randomElement( array( 'male', 'female' ) ) );
-		$lastname    = self::$faker->lastName();
-		$company     = self::$faker->company();
-		$address1    = self::$faker->buildingNumber() . ' ' . self::$faker->streetName();
-		$address2    = self::$faker->streetAddress();
-		$city        = self::$faker->city();
-		$state       = self::$faker->stateAbbr();
-		$postcode    = self::$faker->postcode();
-		$countrycode = self::$faker->countryCode();
-		$phone       = self::$faker->e164PhoneNumber();
-		$customer    = new \WC_Customer();
+        /**
+    PERSON 
+*/
+        $person['billing']['firstname'] = self::$faker->firstName(self::$faker->randomElement(array( 'male', 'female' )));
+        $person['billing']['lastname']  = self::$faker->lastName();
 
-		$customer->set_props( array(
-			'date_created'        => null,
-			'date_modified'       => null,
-			'email'               => $email,
-			'first_name'          => $firstname,
-			'last_name'           => $lastname,
-			'display_name'        => $firstname,
-			'role'                => 'customer',
-			'username'            => $username,
-			'password'            => self::$faker->password(),
-			'billing_first_name'  => $firstname,
-			'billing_last_name'   => $lastname,
-			'billing_company'     => $company,
-			'billing_address_1'   => $address1,
-			'billing_address_2'   => $address2,
-			'billing_city'        => $city,
-			'billing_state'       => $state,
-			'billing_postcode'    => $postcode,
-			'billing_country'     => $countrycode,
-			'billing_email'       => $email,
-			'billing_phone'       => $phone,
-			'shipping_first_name' => $firstname,
-			'shipping_last_name'  => $lastname,
-			'shipping_company'    => $company,
-			'shipping_address_1'  => $address1,
-			'shipping_address_2'  => $address2,
-			'shipping_city'       => $city,
-			'shipping_state'      => $state,
-			'shipping_postcode'   => $postcode,
-			'shipping_country'    => $countrycode,
-			'is_paying_customer'  => false,
-		) );
+        if (self::$faker->randomFloat(0, 0, 1) == 1 ) {
+            $person['shipping']['firstname'] = self::$faker->firstName(self::$faker->randomElement(array( 'male', 'female' )));
+            $person['shipping']['lastname']  = self::$faker->lastName();
+        } else {
+            $person['shipping']['firstname'] = $person['billing']['firstname'];
+            $person['shipping']['lastname']  = $person['billing']['lastname'];
+        }
 
-		if ( $save ) {
-			$customer->save();
-		}
+        /*COMPANY */
+        $relationType = self::$faker->randomElements($array = array( 'B2B', 'C2C', 'C2B', 'B2C' ), $count = 1);
 
-		return $customer;
-	}
+        switch ( $relationType[0] ) {
+        case 'B2B':
+            $company['billing']['company_name'] = self::$faker->company();
+            if (self::$faker->randomFloat(0, 0, 1) == 1 ) {
+                $company['shipping']['company_name'] = self::$faker->company();
+            } else {
+                $company['shipping']['company_name'] = $company['billing']['company_name'];
+            }
+
+            break;
+        case 'C2C':
+            $company['billing']['company_name']  = '';
+            $company['shipping']['company_name'] = '';
+            break;
+        case 'B2C':
+            $company['billing']['company_name']  = self::$faker->company();
+            $company['shipping']['company_name'] = '';
+            break;
+        case 'C2B':
+            $company['billing']['company_name']  = '';
+            $company['shipping']['company_name'] = self::$faker->company();
+            break;
+        default:
+            break;
+        }
+        /*ADDRESS */
+        $address['billing']['address0'] = self::$faker->buildingNumber() . ' ' . self::$faker->streetName();
+        $address['billing']['address1'] = self::$faker->streetAddress();
+        $address['billing']['city']     = self::$faker->city();
+        $address['billing']['state']    = self::$faker->stateAbbr();
+        $address['billing']['postcode'] = self::$faker->postcode();
+        $address['billing']['country']  = self::$faker->countryCode();
+        $address['billing']['phone']    = self::$faker->e164PhoneNumber();
+        $address['billing']['email']    = $email;
+
+        if (self::$faker->randomFloat(0, 0, 1) == 1 ) {
+            $address['shipping']['address0'] = self::$faker->buildingNumber() . ' ' . self::$faker->streetName();
+            $address['shipping']['address1'] = self::$faker->streetAddress();
+            $address['shipping']['city']     = self::$faker->city();
+            $address['shipping']['state']    = self::$faker->stateAbbr();
+            $address['shipping']['postcode'] = self::$faker->postcode();
+            $address['shipping']['country']  = self::$faker->countryCode();
+            $address['shipping']['phone']    = self::$faker->e164PhoneNumber();
+        } else {
+            $address['shipping']['address0'] = $address['billing']['address0'];
+            $address['shipping']['address1'] = $address['billing']['address1'];
+            $address['shipping']['city']     = $address['billing']['city'];
+            $address['shipping']['state']    = $address['billing']['state'];
+            $address['shipping']['postcode'] = $address['billing']['postcode'];
+            $address['shipping']['country']  = $address['billing']['country'];
+        }
+
+        $customer = new \WC_Customer();
+
+        $customer->set_props(
+            array(
+            'date_created'        => null,
+            'date_modified'       => null,
+            'email'               => $email,
+            'first_name'          => $person['billing']['firstname'],
+            'last_name'           => $person['billing']['lastname'],
+            'display_name'        => $person['billing']['firstname'],
+            'role'                => 'customer',
+            'username'            => $username,
+            'password'            => self::$faker->password(),
+            'billing_first_name'  => $person['billing']['firstname'],
+            'billing_last_name'   => $person['billing']['lastname'],
+            'billing_company'     => $company['billing']['company_name'],
+            'billing_address_0'   => $address['billing']['address0'],
+            'billing_address_1'   => $address['billing']['address1'],
+            'billing_city'        => $address['billing']['city'],
+            'billing_state'       => $address['billing']['state'],
+            'billing_postcode'    => $address['billing']['postcode'],
+            'billing_country'     => $address['billing']['country'],
+            'billing_email'       => $address['billing']['email'],
+            'billing_phone'       => $address['billing']['phone'],
+            'shipping_first_name' => $person['shipping']['firstname'],
+            'shipping_last_name'  => $person['shipping']['lastname'],
+            'shipping_company'    => $company['shipping']['company_name'],
+            'shipping_address_0'  => $address['shipping']['address0'],
+            'shipping_address_1'  => $address['shipping']['address1'],
+            'shipping_city'       => $address['shipping']['city'],
+            'shipping_state'      => $address['shipping']['state'],
+            'shipping_postcode'   => $address['shipping']['postcode'],
+            'shipping_country'    => $address['shipping']['country'],
+            'is_paying_customer'  => false,
+            ) 
+        );
+
+        if ($save ) {
+            $customer->save();
+        }
+
+        return $customer;
+    }
 
 }
