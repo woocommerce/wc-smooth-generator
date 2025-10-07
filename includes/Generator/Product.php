558cdccff4abc69cbb56e03325e799684cf1c11f
Brian <brian@brianhaas.li>
2025-10-07 18:38:08 +0200

    Update includes/Generator/Product.php
    Co-authored-by: Justin P <228780+layoutd@users.noreply.github.com>

--- includes/Generator/Product.php
+++ includes/Generator/Product.php
@@ -388,7 +388,7 @@ class Product extends Generator {
 			'global_unique_id'   => self::$faker->randomElement( [ self::$faker->ean13, self::$faker->isbn10 ] ),
 			'regular_price'      => $price,
 			'sale_price'         => $sale_price,
-			'cogs_value'         => round($price * (1 - self::$faker->numberBetween(15, 60) / 100), 2),
+			'cogs_value'         => round( $price * ( 1 - self::$faker->numberBetween( 15, 60 ) / 100 ), 2 ),
 			'date_on_sale_from'  => '',
 			'date_on_sale_to'    => self::$faker->iso8601( date( 'c', strtotime( '+1 month' ) ) ),
 			'total_sales'        => self::$faker->numberBetween( 0, 10000 ),
